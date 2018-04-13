5613dd5bf8377e4cb119c9dbb648fd867a19afda
David Atienza <daries100@gmail.com>
2018-04-13 15:31:01 +0200

    Benchmarks included.

--- benches/bench_main.rs
+++ benches/bench_main.rs
@@ -7,6 +7,7 @@ use criterion::Criterion;
 use ndarray::{Array, ArrayD, ArrayViewD, ArrayBase, Axis};
 use rand::{Rand,Rng};
 
+/// Returns an array view with `n_axis` axes with length 1 at the start of the shape.
 fn insert_axes_first<S>(array : &ArrayD<S>, n_axis: usize) -> ArrayViewD<S> {
     let mut a = array.view();
     for _ in 0..n_axis {
@@ -15,6 +16,7 @@ fn insert_axes_first<S>(array : &ArrayD<S>, n_axis: usize) -> ArrayViewD<S> {
     a
 }
 
+/// Returns an array view with `n_axis` axes with length 1 at the end of the shape.
 fn insert_axes_end<S>(array : &ArrayD<S>, n_axis: usize) -> ArrayViewD<S> {
     let mut a = array.view();
     for _ in 0..n_axis {
@@ -24,6 +26,7 @@ fn insert_axes_end<S>(array : &ArrayD<S>, n_axis: usize) -> ArrayViewD<S> {
     a
 }
 
+/// Generates a random array with the given shape.
 fn random_array<S: Rand>(shape: Vec<usize>) -> ArrayD<S>
 {
     let n_elements = shape.iter().fold(1, |accum, &elem| accum * elem);
@@ -37,16 +40,19 @@ fn random_array<S: Rand>(shape: Vec<usize>) -> ArrayD<S>
 
 fn product(c: &mut Criterion) {
     c.bench_function_over_inputs("product",  |b, &size| {
+        // Number of axes to add on each matrix.
         let new_axes = size / 2;
         let shape = vec![2; size];
         let zeros = random_array::<f64>(shape.clone());
-        let expected_card = vec![2; size + new_axes];
         let ones = random_array::<f64>(shape);
+        // Expected cardinality of the product of the two arrays.
+        let expected_card = vec![2; size + new_axes];
 
         b.iter_with_setup(|| (zeros.clone(), ones.clone(), expected_card.clone()),
                           |(zeros, ones, expected_card)| {
                                 let zeros_view = insert_axes_first(&zeros,new_axes);
                                 let ones_view = insert_axes_end(&ones,new_axes);
+                              // Broadcasting the left hand side first.
                               let zeros_view = zeros.broadcast(expected_card).unwrap();
                               let _array = &zeros_view*&ones_view;
                           })
